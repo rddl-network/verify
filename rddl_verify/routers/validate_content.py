462066904a36b4e7972a41f20b740da8deaf53a9
Jürgen Eckel <juergen@riddleandcode.com>
2023-05-10 10:53:42 +0200

    blackified
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- rddl_verify/routers/validate_content.py
+++ rddl_verify/routers/validate_content.py
@@ -9,38 +9,40 @@ router = APIRouter(
     responses={404: {"detail": "Not found"}},
 )
 
-def convert_key_sig( pub_key: str, signature: bytes ):
+
+def convert_key_sig(pub_key: str, signature: bytes):
     try:
-        pub_key_bytes =  codecs.decode(pub_key, 'hex_codec')
+        pub_key_bytes = codecs.decode(pub_key, "hex_codec")
     except ValueError as e:
         raise HTTPException(status_code=404, detail=f"pub key: Invalid input parameter {e}")
     try:
-        signature_bytes =  codecs.decode(signature, 'hex_codec')
+        signature_bytes = codecs.decode(signature, "hex_codec")
     except ValueError as e:
         raise HTTPException(status_code=404, detail=f"signature: Invalid input parameter {e}")
     return pub_key_bytes, signature_bytes
 
+
 @router.post("/json", response_model=ValidationResult)
 async def validate_signature(pub_key: str, signature: str, data: dict):
-    pub_key_bytes, signature_bytes = convert_key_sig( pub_key, signature )
+    pub_key_bytes, signature_bytes = convert_key_sig(pub_key, signature)
 
     return verify_signature.validate_signature_json_data(pub_key_bytes, signature_bytes, data)
 
 
 @router.get("/string", response_model=ValidationResult)
 async def validate_signature(pub_key: str, signature: str, data: str):
-    pub_key_bytes, signature_bytes = convert_key_sig( pub_key, signature )
+    pub_key_bytes, signature_bytes = convert_key_sig(pub_key, signature)
 
     return verify_signature.validate_signature_data_string(pub_key_bytes, signature_bytes, data)
 
 
 @router.get("/hash", response_model=ValidationResult)
 async def validate_signature(pub_key: str, signature: str, data: str):
-    pub_key_bytes, signature_bytes = convert_key_sig( pub_key, signature )
+    pub_key_bytes, signature_bytes = convert_key_sig(pub_key, signature)
     try:
-        data_hash_bytes =  codecs.decode(data, 'hex_codec')
+        data_hash_bytes = codecs.decode(data, "hex_codec")
     except ValueError as e:
         raise HTTPException(status_code=404, detail=f"data: Invalid input parameter {e}")
-    
+
     result = verify_signature.validate_signature_data_hash(pub_key_bytes, signature_bytes, data_hash_bytes)
     return result
--- tests/test_api.py
+++ tests/test_api.py
@@ -4,19 +4,22 @@ from rddl_verify.main import app
 
 client = TestClient(app)
 
-pub_key = '03c150061989643d77162902b725409087959f15914649d4f06b6cc3f8c87bb238'
-sig= '68fba9c12a74bbd3399b0efe9c924b884a2e037d827b147ec9ddd9c26217163a1776e73ffbf0b4655ac70ca565310d307f86a0c9c71e0c8926b47fdfafc8a7fc'
-hash= '2fc3dbae8a4db255781b38340fd1e372ac0cb6d3bd4d9eeba417227d1d5f7b38'
-obj_str= '{"a": "dear Test", "b": "welcome"}'
+pub_key = "03c150061989643d77162902b725409087959f15914649d4f06b6cc3f8c87bb238"
+sig = "68fba9c12a74bbd3399b0efe9c924b884a2e037d827b147ec9ddd9c26217163a1776e73ffbf0b4655ac70ca565310d307f86a0c9c71e0c8926b47fdfafc8a7fc"
+hash = "2fc3dbae8a4db255781b38340fd1e372ac0cb6d3bd4d9eeba417227d1d5f7b38"
+obj_str = '{"a": "dear Test", "b": "welcome"}'
 json_dict = {"a": "dear Test", "b": "welcome"}
+
+
 def test_hash():
     response = client.get(f"validate/hash?pub_key={pub_key}&signature={sig}&data={hash}")
     assert response.status_code == 200
     assert response.json()["json_obj"] == {}
     assert response.json()["data_digest"] == hash
-    assert response.json()["data_str"] == ''
+    assert response.json()["data_str"] == ""
     assert response.json()["is_valid"] == True
-    
+
+
 def test_str():
     response = client.get(f"validate/string?pub_key={pub_key}&signature={sig}&data={obj_str}")
     assert response.status_code == 200
@@ -24,8 +27,8 @@ def test_str():
     assert response.json()["data_digest"] == hash
     assert response.json()["data_str"] == obj_str
     assert response.json()["is_valid"] == True
-    
-    
+
+
 def test_json():
     response = client.post(f"validate/json?pub_key={pub_key}&signature={sig}", json=json_dict)
     assert response.status_code == 200
